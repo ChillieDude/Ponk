e63c2d7b7eb78ad757279c36fdfe6dbe92a84efa
ChillieDude <chillieman224@aim.com>
2015-04-18 17:47:13 -0400

    Create Globals.h

---
+++ Globals.h
@@ -0,0 +1,20 @@
+#ifndef GLOBALS_H_INCLUDED
+#define GLOBALS_H_INCLUDED
+
+
+#include<SDL.h>
+#include<iostream>
+//Declare your classes:
+
+
+//Declare your Functions here:
+void Play();
+
+//Declare Global Variables:
+extern SDL_Window* ;
+extern SDL_Render* ;
+
+extern const SCREEN_W;
+extern const SCREEN_H;
+
+#endif
